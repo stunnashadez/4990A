10651d9946dd1aa43d2f8fe2bd78ec836e29d3c5
Oprescu <radu.oprescu@yahoo.com>
2021-03-22 15:24:57 +0200

    Final Version Added

---
+++ Final Version/A_ Pathfinding/Assets/DFS.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.Threading;
+public class DFS : MonoBehaviour
+{
+
+ public Transform seeker, target;
+    
+    Gridd grid; //reference to our grid 
+
+    void Awake()
+    {
+        grid = GetComponent<Gridd>();
+    }
+
+    void Update()
+    {
+    }
+
+    public void Run(){
+        if(gui.isVisualEnabled){
+            gui.isVisualEnabled = false;
+            StartCoroutine(FindPath(seeker.position, target.position));
+            gui.isVisualEnabled = true;
+            StartCoroutine(FindPath(seeker.position, target.position));
+        }
+        else
+            StartCoroutine(FindPath(seeker.position, target.position));
+    }
+
+    IEnumerator FindPath(Vector3 startPos, Vector3 targetPos)  //start position and target position
+    {
+        gui.isRunning = true;
+        Stopwatch sw = new Stopwatch(); //create a stopwatch to time the speed of the implementation of the algorithm using heap method 
+        sw.Start(); //start timer before starting pathfinding 
+
+        //need to convert world positions into node  
+        bool foundWalkable = false;
+        Node startNode = grid.NodeFromWorldPoint(startPos); //create a start node
+        Node targetNode = grid.NodeFromWorldPoint(targetPos); //create a target node
+        HashSet<Node> closeSet = new HashSet<Node>(); //use a hashset for our closed set
+        Stack<Node> current_path = new Stack<Node>();
+        List<Node> path = new List<Node>(); //create a list of nodes called path
+        current_path.Push(startNode);
+        grid.path = new List<Node>();
+        while (current_path.Count != 0) 
+        {
+            Node currentNode = current_path.Peek();//set current node to the first element in the openset
+            if(gui.isVisualEnabled){
+                grid.path.Add(currentNode);
+                if (closeSet.Count % 15 == 0)
+                    yield return new WaitForSeconds(gui.visualdelay);
+            }
+            closeSet.Add(currentNode); //add the currentNode to the closedSet
+
+            if(currentNode == targetNode) //corrent path has been found
+            {
+                sw.Stop(); //end timer when path has been found 
+                while(current_path.Count != 0){
+                    path.Add(current_path.Pop());
+                }
+                grid.path = path;
+                if(gui.isVisualEnabled == false)
+                    gui.results = "Path found: " + sw.ElapsedMilliseconds + " ms " + " | " + "Distance : " + grid.path.Count;
+                gui.isRunning = false;
+                yield break;
+            }
+
+            foundWalkable= false;
+            foreach (Node neighbour in grid.GetNeighbours(currentNode)) //for each neighbour of the current node 
+            {
+                if(neighbour.walkable && !closeSet.Contains(neighbour)) //if the neighbouring node is not walkable or in the closed set
+                {
+                    current_path.Push(neighbour);
+                    foundWalkable = true;
+                    break;
+                }
+
+            }
+            if(!foundWalkable){
+                if(gui.isVisualEnabled){
+                    List<Node> tmp = grid.path;
+                    tmp.Remove(current_path.Peek());
+                    tmp.RemoveAt(tmp.Count -1);
+                    grid.path = tmp;
+                }
+                current_path.Pop();
+                }
+        }
+        gui.results = "Target not Reachable!";
+        gui.isRunning = false;
+    }
+    
+}
---
+++ Final Version/A_ Pathfinding/Assets/gui.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+/*
+    This is the gui class that handles controls
+    It allows the user to choose between algorithms to execute,
+    change levels, visualize pathfinding process and see results
+    from execution.
+
+    It also allows user to randomize seeker and target position
+*/
+public class gui : MonoBehaviour
+{
+    public Rect windowRect = new Rect(20, 20, 430, 400);
+    public static float visualdelay = 0.0F;                 //This controls the loop delay for visual pathfinding (i.e. it slows down the search)
+    public static bool isVisualEnabled = false;             //This controls if visual pathfinding is enabled (i.e. whether you want to see the process or not)
+    public static string results = "Result: ";              //This string displays the algorithm results on the gui
+    public Transform seeker, target;                        //Reference to seeker GameObject and target GameObject
+    public Transform Camera;                                //Reference to camera (this is needed to change camera perspective)
+    public static bool isRunning = false;                   //Checks if algorithm is running, this is to avoid launching 2 pathfinding algo's at the same time
+    public static int cameraPOS = 0;                        //Saves the camera's current position (preset, 1 = some coordinates, preset 2 = some other coords)
+    public static int level = 0;                            //Saves the current level loaded
+
+    AStar aStar;                                            //Object declaration for pathfinding algorithms
+    Gridd grid;
+    DFS dfs;
+    BFS bfs;
+
+    void Awake()
+    {
+        aStar = GetComponent<AStar>();
+        dfs = GetComponent<DFS>();
+        bfs = GetComponent<BFS>();
+        grid = GetComponent<Gridd>();
+    }
+
+    void OnGUI()
+    {
+        windowRect = GUI.Window(0, windowRect, drawWindow, "");
+
+    }
+
+    void Update()
+    {
+    }
+
+    void drawWindow(int windowID)
+    {
+
+        GUI.Label(new Rect(10, 10, 200, 35), "Run: ");
+
+        if (GUI.Button(new Rect(10, 30, 100, 50), "A* Algorithm") && isRunning == false)
+        {
+            grid.CreateGrid();   //Refreshes grid in case user moved objects
+            aStar.Run();
+        }
+
+        if (GUI.Button(new Rect(10, 90, 100, 50), "DFS Algorithm") && isRunning == false)
+        {
+            grid.CreateGrid();  //Refreshes grid in case user moved objects
+            dfs.Run();
+        }
+
+        if (GUI.Button(new Rect(10, 150, 100, 50), "BFS Algorithm") && isRunning == false)
+        {
+            grid.CreateGrid();  //Refreshes grid in case user moved objects
+            bfs.Run();
+        }
+        if (GUI.Button(new Rect(260, 345, 100, 50), "Change Level") && isRunning == false)
+        {
+            level++;
+            switch (level)
+            {
+                case 1:
+                    SceneManager.LoadScene(level);
+                    break;
+                default:
+                    SceneManager.LoadScene(0);
+                    level = 0;
+                    break;
+
+            }
+        }
+        GUI.Label(new Rect(200, 10, 200, 35), "Options: ");
+
+        if (GUI.Button(new Rect(200, 30, 150, 50), "Change Camera"))
+        {
+            cameraPOS++;
+
+            switch (cameraPOS)
+            {
+                case 1:
+                    Camera.position = new Vector3(-34F, 144.18F, -27.8F);
+                    Camera.rotation = Quaternion.Euler(79.30F, 5.3F, 5F);
+                    break;
+                case 2:
+                    Camera.position = new Vector3(-54.51F, 34.2F, -74.38F);
+                    Camera.rotation = Quaternion.Euler(16.801F, 39.013F, 0.17F);
+                    break;
+                default:
+                    Camera.position = new Vector3(-19.6F, 99.1F, -84.4F);
+                    Camera.rotation = Quaternion.Euler(51.73F, 1.58F, 1.01F);
+                    cameraPOS = 0;
+                    break;
+
+            }
+        }
+
+        if (GUI.Button(new Rect(200, 90, 150, 50), "Randomize Seeker") && isRunning == false)
+        {
+            grid.CreateGrid();  //Refreshes grid in case user moved objects
+            grid.path = new List<Node>(); //Reset displayed grid
+            while (true)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f));
+                Node seekerNode = grid.NodeFromWorldPoint(randomPos);
+                if (seekerNode.walkable)
+                {
+                    seeker.position = randomPos;
+                    break;
+                }
+            }
+        }
+
+        if (GUI.Button(new Rect(200, 150, 150, 50), "Randomize Target") && isRunning == false)
+        {
+            grid.CreateGrid();  //Refreshes grid in case user moved objects
+            grid.path = new List<Node>(); //Reset displayed grid
+            while (true)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-50.0f, 50.0f), 0, Random.Range(-50.0f, 50.0f));
+                Node targetNode = grid.NodeFromWorldPoint(randomPos);
+                if (targetNode.walkable)
+                {
+                    target.position = randomPos;
+                    break;
+                }
+            }
+        }
+
+        GUI.Label(new Rect(10, 245, 200, 50), "Visualization Speed: ");
+        visualdelay = GUI.HorizontalSlider(new Rect(10, 270, 200, 50), visualdelay, 0.05F, 0.00F);
+        isVisualEnabled = GUI.Toggle(new Rect(10, 220, 200, 50), isVisualEnabled, "View Pathfinding Process");
+
+
+        GUI.Label(new Rect(10, 345, 300, 50), results);
+    }
+}
